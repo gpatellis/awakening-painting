664a607836fbb13558010227c0b7448be8f299d6
gpatellis <gregpat10@gmail.com>
2023-07-24 11:17:36 -0400

    fix checkout status on refresh, ship opt styling

--- src/app/layout/painting-checkout/ordering-status/ordering-status.component.ts
+++ src/app/layout/painting-checkout/ordering-status/ordering-status.component.ts
@@ -2,16 +2,44 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ORDERING_STATUS } from '../painting-checkout.model';
 import { OrderingStatusService } from './ordering-status.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-ordering-status',
   templateUrl: './ordering-status.component.html',
   styleUrls: ['./ordering-status.component.scss']
 })
-export class OrderingStatusComponent {
+export class OrderingStatusComponent implements OnInit{
 
   orderingStatus: Observable<ORDERING_STATUS> = this.orderingStatusService.OrderingStatus$;
   ORDERING_STATUS = ORDERING_STATUS;
 
-  constructor(private orderingStatusService: OrderingStatusService) { }
+  constructor(
+    private orderingStatusService: OrderingStatusService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.checkRouteOnInit()
+  }
+
+  checkRouteOnInit() {
+    switch(this.router.url) { 
+      case '/checkout/shipping': { 
+        this.orderingStatusService.OrderingStatus$.next(ORDERING_STATUS.shipping)
+        break; 
+      } 
+      case '/checkout/payment': { 
+        this.orderingStatusService.OrderingStatus$.next(ORDERING_STATUS.payment)
+         break; 
+      } 
+      case '/checkout/confirmation': { 
+        this.orderingStatusService.OrderingStatus$.next(ORDERING_STATUS.confirmation)
+         break; 
+      } 
+      default: { 
+        this.orderingStatusService.OrderingStatus$.next(ORDERING_STATUS.shipping)
+        break; 
+      } 
+   } 
+  }
 }
