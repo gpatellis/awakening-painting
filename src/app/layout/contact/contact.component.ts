fb1c9720533f0aeb414c15b59a10bb0c11b0fd9b
gpatellis <gregpat10@gmail.com>
2023-10-12 13:54:34 -0400

    add title to contact and home pages

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingIndicatorService } from 'src/app/shared-services/loading-indicator/loading-indicator.service';
 
 @Component({
   selector: 'ap-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  constructor(private loadingIndicatorService: LoadingIndicatorService) {}
+
+  ngOnInit(): void {
+    this.loadingIndicatorService.hide();
+  }
 
 }
