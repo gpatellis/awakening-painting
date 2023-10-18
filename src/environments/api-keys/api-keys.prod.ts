a399d4c5617ffb4775adbe04cb58a004f92294ec
gpatellis <gregpat10@gmail.com>
2023-10-18 10:40:15 -0400

    update git ignore

--- src/environments/api-keys/api-keys.prod.ts
+++
@@ -1,8 +0,0 @@
-export const apiKeys = { 
-    googleMapsApi: {
-        apiKey: "AIzaSyC9s0coG4ziTZ9etSHtm_FWwrPRPX9c2eE"
-    },
-    stripe: {
-        publicKey: "pk_live_51NXQnKAFGO2wW8pWFyP20bM98fQyXLMrLJvOGzAXyANNHKRlkrfLeU6MJEDE0e14Jlddyb4rYWWTcceLazoz9cD4001JpBC1eE",
-    }
-}
--- src/environments/api-keys/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const apiKeys = { 
-    googleMapsApi: {
-        apiKey: "AIzaSyC9s0coG4ziTZ9etSHtm_FWwrPRPX9c2eE"
-      },
-      stripe: {
-        publicKey: "pk_test_51NXQnKAFGO2wW8pWFNwPdMIGFRwXIHFRsKqOThn95bOAuxNsSuKBS6h3o7rSs7SSoS7NTPimqmKTdaJYVqVzcwG800GZh6hL3a",
-      }
-}
